3096831408a56395b898ef64eb9a67841496b038
Matthew Denaburg <matthew.denaburg1@gmail.com>
2021-06-13 13:16:20 -0400

    various pylint fixes

--- veracross_api/veracross.py
+++ veracross_api/veracross.py
@@ -26,15 +26,18 @@ Returned will be a dictionary of data.
 """
 
 
-import requests
+from urllib import parse
 import math
 import time
-from urllib import parse
 
-__author__ = "Forrest Beck"
+import requests
 
 
-class Veracross(object):
+__author__ = "Forrest Beck,  Matthew Denaburg"
+
+
+class Veracross:
+    """This class provides an easy interface to the Veracross API for python."""
 
     def __init__(self, config):
         self.rate_limit_remaining = 300
@@ -42,7 +45,7 @@ class Veracross(object):
         self.__session = requests.Session()
 
         if 'school_short_name' in config:
-            self.school_short_name = str(config['school_short_name'))
+            self.school_short_name = str(config['school_short_name'])
             self.api_url = 'https://api.veracross.com/{}/v2/'.format(self.school_short_name)
 
         if 'vcurl' in config:
@@ -55,61 +58,62 @@ class Veracross(object):
             raise KeyError('Credentials not provided')
 
     def __repr__(self):
-        return "VC API connected to " + str(self.api_url) + " as " + str(self.vcuser)
+        return f"VC API connected to {self.api_url} as {self.vcuser}"
 
     def set_timers(self, limit_remaining, limit_reset):
         """
         Sets the rate limits
+
         :param limit_remaining: Count of API calls remaining from header X-Rate-Limit-Remaining
         :param limit_reset: Reset Timer from header X-Rate-Limit-Reset
         :return: None
+
         """
         self.rate_limit_remaining = int(limit_remaining)
         self.rate_limit_reset = int(limit_reset)
+
         if self.rate_limit_remaining == 1:
             time.sleep(self.rate_limit_reset + 1)
 
     def pull(self, source, parameters=None):
         """
-        Get Veracross Data with pagination
+        Get Veracross data with pagination
+
         :param source: VC Source (households, facstaff, facstaff/99)
         :param parameters: Optional API parameters normally in GET request
         :return: records in a list of dictionaries
         """
-        try:
-            if not self.__session.auth:
-                raise RuntimeError("Not connected.")
 
-            if parameters is not None:
-                s = self.api_url + source + ".json?" + parse.urlencode(parameters, safe=':-,')
-            else:
-                s = self.api_url + source + ".json"
+        if not self.__session.auth:
+            raise RuntimeError("Not connected.")
 
-            r = self.__session.get(s)
+        if parameters is not None:
+            s = f"{self.api_url}{source}.json?" + parse.urlencode(parameters, safe=':-,')
+        else:
+            s = f"{self.api_url}{source}.json"
 
-            if r.status_code == 200:
-                if 'X-Total-Count' in r.headers:
-                    pages = math.ceil(int(r.headers['X-Total-Count']) / 100)
+        r = self.__session.get(s)
+        records = []
+
+        if r.status_code == 200:
+            if 'X-Total-Count' in r.headers:  # if there are multiple pages
+                pages = math.ceil(int(r.headers['X-Total-Count']) / 100)
+            else:  # just return the results
+                self.set_timers(r.headers['X-Rate-Limit-Remaining'],
+                                r.headers['X-Rate-Limit-Reset'])
+                return r.json()
+
+            page = 1
+
+            while page <= pages:
+                self.set_timers(r.headers['X-Rate-Limit-Remaining'],
+                                r.headers['X-Rate-Limit-Reset'])
+                if parameters is None:
+                    r = self.__session.get(s + "?page=" + str(page))
                 else:
-                    self.set_timers(r.headers['X-Rate-Limit-Remaining'],
-                                    r.headers['X-Rate-Limit-Reset'])
-                    return r.json()
+                    r = self.__session.get(s + "&page=" + str(page))
 
-                page = 1
-                records = []
+                records += r.json()
+                page += 1
 
-                while page <= pages:
-                    self.set_timers(r.headers['X-Rate-Limit-Remaining'],
-                                    r.headers['X-Rate-Limit-Reset'])
-                    if parameters is None:
-                        r = self.__session.get(s + "?page=" + str(page))
-                    else:
-                        r = self.__session.get(s + "&page=" + str(page))
-
-                    records += r.json()
-                    page += 1
-                return records
-            else:
-                return None
-        except:
-            return None
+        return records
