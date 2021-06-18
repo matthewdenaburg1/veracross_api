26f1119dc853a42b32a579ec66639d9782b5cf1e
Matthew Denaburg <matthew.denaburg1@gmail.com>
2021-06-18 13:21:37 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from distutils.core import setup
 import setuptools
 from os import path
 
+
+__version__ = '1.0.3'
+
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -9,7 +13,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='veracross_api',
     packages=['veracross_api'],
-    version='1.0.2',
+    version=__version__,
     description='Simple library for interacting with the Veracross API',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -17,7 +21,7 @@ setup(
     author='Forrest Beck, Matthew Denaburg',
     author_email='forrest.beck@da.org, matthew.denaburg@ssfs.org',
     url='https://bitbucket.org/ssfs_tech/veracross_api',
-    download_url='https://bitbucket.org/ssfs_tech/veracross_api/get/v1.0.2.tar.gz',
+    download_url=f'https://bitbucket.org/ssfs_tech/veracross_api/get/v{__version__}.tar.gz',
     keywords=['Veracross', 'API'],
     install_requires=['requests'],
     classifiers=[
